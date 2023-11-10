7c3ae2d472500026bb1281e0d1083010af0be1b1
jnk <jnk626@pm.me>
2023-11-10 17:25:51 +0100

    minor bug fix

--- microservices/anagraficaService/src/main/java/it/contrader/anagraficaservice/dto/AnagraficaDTO.java
+++ microservices/anagraficaService/src/main/java/it/contrader/anagraficaservice/dto/AnagraficaDTO.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class AnagraficaDTO {
+public class AnagraficaDTO implements Serializable {
 
     private Long id;
 
--- microservices/authenticationService/src/main/java/it/contrader/authenticationservice/controller/AuthController.java
+++ microservices/authenticationService/src/main/java/it/contrader/authenticationservice/controller/AuthController.java
@@ -6,9 +6,12 @@ import it.contrader.authenticationservice.dto.LoginDTO;
 import it.contrader.authenticationservice.dto.MessageResponse;
 import it.contrader.authenticationservice.dto.SignupDTO;
 import it.contrader.authenticationservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,8 @@ public class AuthController {
     @Autowired
     private StreamBridge streamBridge;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginDTO loginDTO) {
         return ResponseEntity.ok()
@@ -34,8 +39,9 @@ public class AuthController {
     public ResponseEntity<?> registerUser(@RequestBody SignupDTO signUpRequest) {
         try {
             userService.registerUser(signUpRequest);
-            // bindingName must match the application.yml property
-            streamBridge.send("output-out-0", signUpRequest.getAnagrafica());
+            // bindingName must match the application.yml property of the binding
+            streamBridge.send("output-out-0", signUpRequest.getAnagrafica(), MimeType.valueOf("application/json"));
+            LOGGER.info("Successfully sent: {}", signUpRequest.getAnagrafica());
             return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
         } catch (UsernameAlreadyInUseException | EmailAlreadyInUseException ex) {
             return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
